da8c5564d6d752a6ef09e9c36150e95ac02624d4
Pedro <p-cardozo@hotmail.com>
2024-07-31 11:54:52 -0300

    fix(app): change module import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { fetchAuthSession } from 'aws-amplify/auth';
+import { getCurrentUser } from 'aws-amplify/auth';
 
 
 export async function middleware(request: NextRequest) {
     const response = NextResponse
 
-    const session = await fetchAuthSession();
-
-    // console.log("username", username);
-    // console.log("user id", userId);
-    // console.log("sign-in details", signInDetails);
+    const _ = await getCurrentUser();
     
-    console.log(request.cookies.getAll())
-
-    response.next().cookies.set('vercel', 'fast')
     response.next().cookies.set({
       name: 'vercel',
       value: 'fast',
